45e25e6afe71b54af56793a4f111c735137c4f5e
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2023-11-18 23:18:17 +0530

    store changes n imports

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,13 @@
 // store.js
 
 import { configureStore } from '@reduxjs/toolkit';
-// import cartReducer from './cartSlice';
+import cartReducer from './cartSlice';
 // import wishlistReducer from './wishlistSlice';
-import productsReducer from './productsSlice';
 
 const store = configureStore({
   reducer: {
-    // cart: cartReducer,
+    cart: cartReducer,
     // wishlist: wishlistReducer,
-    products: productsReducer,
   },
 });
 
