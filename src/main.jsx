9be00a4651542e0112766634649f4c6dcbad9df2
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2023-12-04 03:20:27 +0530

    main change

--- src/main.jsx
+++ src/main.jsx
@@ -22,14 +22,14 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { HashRouter } from 'react-router-dom';
 import  { store, persistor } from './Redux/store.js';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider 
-  clientId="679005434641-jqnp6hd9erjeombqnsdrcflshf556in7.apps.googleusercontent.com" redirectUri="http://localhost:5173/">
+  // <GoogleOAuthProvider 
+  // clientId="679005434641-jqnp6hd9erjeombqnsdrcflshf556in7.apps.googleusercontent.com" redirectUri="http://localhost:5173/">
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <HashRouter basename='/'>
@@ -37,7 +37,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </HashRouter>
     </PersistGate>
   </Provider>
-  </GoogleOAuthProvider>
+  // </GoogleOAuthProvider>
 );
 
 
