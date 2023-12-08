c787c4b7c738998a9a760fa2275ef86f826ccf94
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2023-12-08 22:00:37 +0530

    app build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as  Router, Routes , Route, BrowserRouter, Navigate } from 'react-router-dom'
+import { useNavigate, } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 import React from 'react'
 import Nav from './Pages/Nav.jsx'
 import Products from './Pages/Products.jsx'
@@ -6,14 +9,20 @@ import Login from './Pages/Registrations/Log.jsx'
 import Wishlist from './Pages/Wishlist.jsx'
 import Cart from './Pages/Cart.jsx'
 import Detail from './Pages/Prodctdetail.jsx'
+// import Orders from './Components/Orders/Orders.jsx'
+// import ViewOrder from './Components/Orders/ViewOrder.jsx'
+// import { loadUser } from './Redux/User/userAction.js';
 
-import AddProduct from './Pages/admin/dashboard/page/AddProduct.jsx'
-import Dashboard from './Pages/admin/dashboard/Dashboard.jsx'
+
+
+// import AddProduct from './Pages/admin/dashboard/page/AddProduct.jsx'
+// import Dashboard from './Pages/CMS/Pages/DashPanel.jsx'
 import DashboardTab from './Pages/admin/dashboard/DashboardTab.jsx'
 import UpdateProduct from './Pages/admin/dashboard/page/UpdateProduct.jsx'
 import AdminNav from './Pages/admin/dashboard/page/AdminNav.jsx'
-
-import Signup from './Pages/Registrations/Sign.jsx'
+// import AddProduct from './Pages/CMS/Pages/AddProduct.jsx'
+// import AllProduct from './Pages/CMS/Pages/AllProducts.jsx'
+// import Register from './Pages/Registrations/Register.jsx'
 import Hamburger from './Components/HomePage/hamburger.jsx'
 import User from './Pages/User.jsx'
 import AddDetail from './Components/Checkout/AddDetail.jsx'
@@ -29,10 +38,31 @@ import Saree from './Pages/Saree.jsx'
 import Westrend from './Pages/Western1.jsx'
 import KalkiCollection from './Pages/KalkiCollection.jsx'
 import Search from './Pages/Category/Search.jsx'
+// import SidebarNav from './Pages/CMS/Component/Layout/sidebar.jsx'
+// import Users from './Pages/CMS/Pages/Users.jsx'
+// import ProductsAll from './Components/ProductDisplay/ProductsAll.jsx'
 // import Ethnic from './Pages/Ethnic'
 // import Western from './Pages/Western'
 
-const App = () => {
+const App = ({}) => {
+  const {  isAuthenticated, user  } = useSelector(
+    (state) => state.users
+  )
+  // const  userProfile  = useSelector(
+  //   (state) => state.userProfile
+  // );
+  // console.log(userProfile?.name)
+
+  const navigate = useNavigate()
+   const dispatch = useDispatch();
+  useEffect(() => {
+    if(!isAuthenticated){
+      navigate("/login")
+    }
+   if (isAuthenticated) {
+      dispatch(loadUser(user));
+    }
+  }, [isAuthenticated])
   return (
     <div>
       <MyState>
@@ -40,6 +70,7 @@ const App = () => {
       <Nav/>
       <Routes>
       <Route exact path='/' element={<Products/>}/>
+      <Route exact path='/All' element={<ProductsAll/>}/>
       <Route exact path='/Wishlist' element={<Wishlist/>}/>
       <Route exact path='/Cart' element={<Cart/>}/>
       <Route exact path='/detail' element={<Detail/>}/>
@@ -47,39 +78,64 @@ const App = () => {
       <Route exact path='/User' element={<User/>}/>
       <Route exact path='/AddDetail' element={<AddDetail/>}/>
       <Route exact path='/Payment' element={<Payment/>}/>
-      <Route exact path='/Order' element={<OrderSuccess/>}/> 
+      {/* <Route exact path='/OrderSuccess' element={<OrderSuccess/>}/>  */}
+      {/* <Route exact path='/Orders' element={<Orders/>}/>  */}
+      {/* <Route exact path='/Dashboard' element={<Dashboard/>}/>  */}
 
-              {/* Admin CMS Link */}
+      {/* <Route exact path={`/orderDetails/${id}`} element={<OrderDetails/>}/>  */}
+      {/* y <Route exact path="/orderDetails/:id" element={<ViewOrder/>}/>  */}
+      {/* {isAuthenticated && */}
+      {/* // && user.email === 'anandsaeiou@gmail.com' && */}
+       
+      {/* <Route exact path='/Dashboard' element={<Dashboard />} />)} */}
+      {/* {isAuthenticated  */}
+      {/* //  && userProfile?.role=== "admin"  */}
+      {/* && (<Route exact path='/addProduct' element={<AddProduct />} />) } */}
 
-      <Route exact path='/AddProduct' element={
+      {/* {isAuthenticated 
+      // && userProfile?.role === "admin" 
+      &&   
+      (<Route exact path='/products' element={<AllProduct />} />) }
+
+        {isAuthenticated 
+    // && userProfile?.role === "admin" 
+    && <Route exact path='/orders' element={<Orders />} /> } */}
+
+      {/* {isAuthenticated 
+      // && userProfile?.role === "admin"
+       &&  <Route exact path='/users' element={<Users />} /> } */}
+
+      {/* Admin CMS Link */}
+
+      {/* <Route exact path='/AddProduct' element={
 
       <ProtectedRouteForAdmin>
           <AddProduct/>
       </ProtectedRouteForAdmin>
-      }/> 
+      }/>  */}
 
-      <Route exact path='/Dashboard' element={
+      {/* <Route exact path='/Dashboard' element={
 
-      <ProtectedRouteForAdmin>
+      <ProtectedRouteForAdmin> 
               <Dashboard />
-      </ProtectedRouteForAdmin>}/>
+       </ProtectedRouteForAdmin>}/>  */}
 
-      <Route exact path='/DashboardTab' element={
+      {/* <Route exact path='/DashboardTab' element={
         <ProtectedRouteForAdmin>
           <DashboardTab/>
         </ProtectedRouteForAdmin>
-      }/>
-      <Route exact path='/UpdateProduct' element={
+      }/> */}
+      {/* <Route exact path='/UpdateProduct' element={
       
       <ProtectedRouteForAdmin>
         <UpdateProduct/>
-      </ProtectedRouteForAdmin>}/> 
+      </ProtectedRouteForAdmin>}/>  */}
       
-      <Route exact path='/AdminNav' element={<AdminNav/>}/> 
+      {/* <Route exact path='/AdminNav' element={<AdminNav/>}/>  */}
 
 
               {/* Logins */}
-      <Route exact path='/Signup' element={<Signup/>}/>
+      {/* <Route exact path='/Signup' element={<Register/>}/> */}
       <Route exact path='/Login' element={<Login/>}/>
       <Route exact path='/MobileLogin' element={<MobileLogin/>}/>
       <Route exact path='/MobileLogin/OtpVerify' element={<OtpVerify/>}/>
@@ -96,6 +152,54 @@ const App = () => {
 
             
       </Routes>
+      {/* <Routes > */}
+   {/* {isAuthenticated &&
+  //  && userProfile?.role
+    user.email === 'anandsaeiou@gmail.com' && <Route exact path='/Dashboard' element={<Dashboard />} /> } */}
+   {/* {isAuthenticated 
+   && userProfile?.role
+    === "admin" && <Route exact path='/addProduct' element={<AddProduct />} /> } */}
+    {/* {isAuthenticated 
+    && userProfile?.role
+     === "admin" && <Route exact path='/orders' element={<Orders />} /> }
+   {isAuthenticated 
+   && userProfile?.role
+    === "admin" &&   <Route exact path='/products' element={<AllProduct />} /> }
+    {isAuthenticated 
+    && userProfile?.role
+     === "admin" && <Route exact path='/order/:id' element={<EditOrder />} /> }
+    {isAuthenticated 
+    && userProfile?.role
+     === "admin" &&  <Route exact path='/product/:id' element={<EditProduct />} /> }
+      {isAuthenticated 
+      && userProfile?.role
+       === "admin" &&  <Route exact path='/users' element={<Users />} /> }
+      {isAuthenticated 
+      && userProfile?.role
+       === "admin" &&   <Route exact path='/users/:id' element={<EditUsers />} /> }
+       {isAuthenticated 
+       && userProfile?.role
+        === "admin" &&   <Route exact path='/enquiry' element={<Contacts />} /> }
+        {isAuthenticated 
+        && userProfile?.role
+         === "admin" &&   <Route exact path='/shippingToken' element={<ShippingToken/>} /> }
+         {isAuthenticated 
+         && userProfile?.role
+          === "admin" &&   <Route exact path='/productRequest' element={<ProductRequest />} /> }
+       {isAuthenticated 
+       && userProfile?.role
+        !== "admin" &&   <Route exact path='*' element={<NotAuth />} /> }
+       {isAuthenticated 
+       && userProfile?.role
+        === "admin" && <Route exact path='/addBanners' element={<AddBanners />} /> }
+       {isAuthenticated 
+       && userProfile?.role
+        === "admin" && <Route exact path='/banners' element={<Banners />} /> }
+        {isAuthenticated 
+        && userProfile?.role
+         === "admin" && <Route exact path='/banner/:id' element={<EditBanner />} /> }
+        <Route exact path='/login' element={<Login />} /> */}
+    {/* </Routes> */}
      
       </MyState>
     </div>
@@ -104,25 +208,26 @@ const App = () => {
 export default App
 
 
-export const ProtectedRoute = ({children}) => {
-  const user = localStorage.getItem('user')
-  if(user){
-    return children
-  }else{
-    return <Navigate to={'/login'}/>
-  }
-}
+// export const ProtectedRoute = ({children}) => {
+//   const user = localStorage.getItem('user')
+//   if(user){
+//     return children
+//   }else{
+//     return <Navigate to={'/login'}/>
+//   }
+// }
 
 // admin 
 
-const ProtectedRouteForAdmin = ({children})=> {
-  const admin = JSON.parse(localStorage.getItem('user'))
+// const ProtectedRouteForAdmin = ({children , user})=> {
+//   // const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email === 'anandsaeiou@gmail.com'){
-    return children
-  }
-  else{
-    return <Navigate to={'/login'}/>
-  }
+//   if(user.email === 'anandsaeiou@gmail.com'){
+//     console.log(user.email)
+//     return children
+//   }
+//   else{
+//     return <Navigate to={'/login'}/>
+  // }
 
-}
+// }
